ca8301a8f92e8bc0bf67475e9973aadae5adb61c
aytacnurulu <aytacnurulu332@gmail.com>
2026-02-22 18:36:50 +0400

    added trash and open icon

--- assets/js/admin/category.js
+++ assets/js/admin/category.js
@@ -1,7 +1,24 @@
+const categoryModal = new bootstrap.Modal(
+  document.getElementById("categoryModal"),
+);
+const deleteCategoryModal = new bootstrap.Modal(
+  document.getElementById("deleteCategoryModal"),
+);
 
-const categoryModal = new bootstrap.Modal(document.getElementById('categoryModal'));
-const categoriesCreateButton = document.getElementById('categories-create-button');
+const categoriesCreateButton = document.getElementById(
+  "categories-create-button",
+);
+const editCategoryButtons = document.querySelector("#edit-button");
+const deleteCategoryButtons = document.querySelector("#delete-button");
 
-categoriesCreateButton.addEventListener('click', () => {
+categoriesCreateButton.addEventListener("click", () => {
   categoryModal.show();
+});
+
+editCategoryButtons.addEventListener("click", () => {
+  categoryModal.show();
+});
+
+deleteCategoryButtons.addEventListener("click", () => {
+  deleteCategoryModal.show();
 });
